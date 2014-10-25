f3c3ffbf173c4d623a0f697a4d238e15a1d1dbb6
brownec <cbrowne77@gmail.com>
2014-10-25 12:30:20 +0100

    Started lab 6 Delegates, entered name and student id

---
+++ Lab6_Delegates/Lab6_Delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿// Cliff Browne - X00014810
+// EAD Lab6 - Delegates
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
