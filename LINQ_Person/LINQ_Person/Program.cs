29e1026630cb7d4be278e7d08356a44f8f130be3
brownec <cbrowne77@gmail.com>
2014-10-25 15:21:46 +0100

    Implemented the Person Class and created a collection (enum) called Gender

---
+++ LINQ_Person/LINQ_Person/Program.cs
@@ -0,0 +1,36 @@
+﻿// Cliff Browne - X00014810
+// EAD LINQ Sample Lab - Person (Linq to Memory)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Person
+{
+    // Create a collection called Gender to store the sex/gender of the person object
+    enum Gender { m, f };
+
+    // Create a Person Class that stores Person first name, last name and gender(sex)
+    class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public Gender Sex { get; set; }
+
+        // Override the ToString() Method to return Person details
+        public override string ToString()
+        {
+            return FirstName + " " + LastName + " " + Sex;
+        }
+    }
+
+    // Test Class
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("EAD LINQ Person Lab");
+        }
+    }
+}
