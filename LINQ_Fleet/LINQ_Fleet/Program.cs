6aa3f5243476e30d17203c58b201cb949d35e151
brownec <cbrowne77@gmail.com>
2014-10-25 16:09:31 +0100

    Created Car class and setup queries layout

---
+++ LINQ_Fleet/LINQ_Fleet/Program.cs
@@ -0,0 +1,43 @@
+﻿// Cliff Browne - X00014810
+// EAD1 - Lab LINQ - Fleet
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Fleet
+{
+    // Implement a car class and override ToString() as usual
+    public class Car
+    {
+        // A fleet is a collection of cars. Each car has a make, model, registration and engine size (cc)
+        // Create a fleet collection in memory and populate with some cars
+    }
+
+
+    // Test Class
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("*****EAD1 LINQ Lab - Fleet*****");
+
+// Write LINQ queries on the fleet to:
+            // **********QUERY 1. List all cars in ascending registration order**********
+            
+            
+            // **********QUERY 2. List the models for all Mazda cars in the fleet**********
+            
+            
+            // **********QUERY 3. List all cars in descending engine size order**********
+            
+            
+            // **********QUERY 4. List just the make and model for cars whose engine size is 1600 cc**********
+            
+            
+            // **********QUERY 5. Count the number of cars whose engine size is 1600 cc or less**********
+
+        }
+    }
+}
