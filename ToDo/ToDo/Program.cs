84b85826e7bbf80fe2ff39e262e1abd2ca511a82
brownec <cbrowne77@gmail.com>
2014-10-27 16:09:10 +0000

    Implemented the ToDoList class

--- ToDo/ToDo/Program.cs
+++ ToDo/ToDo/Program.cs
@@ -1,81 +1,43 @@
 ﻿// Cliff Browne - X00014810
 // EAD1 - Worksheet8_ToDo List
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 using System.Collections;
-
-// Priority for the ToDoNotePriority collection
-public enum ToDoNotePriority
-{
-    High, Normal, Low
-}
+using System.Xml;
+using System.Text;
+using System.Collections.Generic;
 
 namespace ToDo
 {
-    // 1.	Implement a ToDoNote class which represents a to-do note. The class should contain the following members:
-    public class ToDoNote
-    {
-        // Fields – the subject matter of the note; the due-date (DateTime); and the priority. 
-        private String subject;
-        private DateTime dueDate;
-        // Implement an appropriate type to represent the various priorities
-        private ToDoNotePriority priority; // priority set for completion
-
-        // Constructor – one constructor which sets all three fields to values passed in as parameters
-        // Property – read/write properties for each of the three fields
-        /* Override the inherited ToString method and return a readable string containing full information about the note. 
-        The due date should be displayed as a date without its time portion. */
-    }
-
-    /* 2.	Implement a SerialisedXML interface. 
-     * This interface should contain just one method which takes a filename as an input parameter. 
-     * Classes which implement this interface have to provide an implementation of this method 
-     * i.e. they should provide an implementation of a method to write their contents to an XML file.
-
-    /* The ToDoNote class should implement this interface and therefore should provide a method which writes 
-     * out the contents of a note in XML format to a specified file name passed in as a parameter. 
-     * This can be easily achieved using the XMLTextWriter class. 
-     * Check out the SDK documentation for sample code on how to do this. */
-
-
-    /* 3.	Implement a ToDoList class which represents a to-do-list. 
-            A to-do-list contains to-do items. 
-            This class should contain the following members: */
-
     // ------------------------------------------------------------------------------------------
     // -------------------------------------TO-DO-LIST CLASS-------------------------------------
     // ------------------------------------------------------------------------------------------
     public class ToDoList
     {
         // Fields - the owner of the list; the collection of to-do notes in the list. 
-        private String listOwner; // owner of the ToDoList
+        private String owner; // owner of the ToDoList
         // The collection should be a dynamically sized collection e.g. an ArrayList
-        private ArrayList notes; 
+        private ArrayList notes;
 
         /* Constructor – one constructor which sets the name of the owner of the list, 
         and initialises the collection of to-do notes to empty */
         public ToDoList(String owner)
         {
-            listOwner = owner;
+            Owner = owner;
             notes = new ArrayList(); // creates a new collection of to-do-notes
         }
-           // Property – a read/write property for the owner field
-        public String listOwner
+        // Property – a read/write property for the owner field
+        public String Owner
         {
             get
             {
-                return listOwner;
+                return owner;
             }
             set
             {
-                listOwner = value;
+                owner = value;
             }
         }
-        
+
         /* Indexer – an indexer which allows the notes in the to-do list to be read (not written) 
         using array type indexing 
         i.e. an object which is an instance of the ToDoList can be iterated over like an array. */
@@ -83,7 +45,7 @@ namespace ToDo
         {
             get
             {
-                if((i >= 0) && (i < notes.Count))
+                if ((i >= 0) && (i < notes.Count))
                 {
                     return (ToDoNote)notes[i];
                 }
@@ -100,11 +62,108 @@ namespace ToDo
             notes.Add(note);
         }
     }
-        
-    
-    // ************************************************************
+    ////////////////////////////////TO-DO-LIST END//////////////////////////////////////////////////////////////////
+
+    // Priority for the ToDoNotePriority collection
+    public enum ToDoNotePriority
+    {
+        High, Normal, Low
+    }
+
+    // Interface to indicate that an item is serialsed to XML
+    public interface SerialiseToXML
+    {
+        void WriteToXML(String filename);
+    }
+
+    // 1.	Implement a ToDoNote class which represents a to-do note. The class should contain the following members:
+    /* The ToDoNote class should implement this interface and therefore should provide a method which writes 
+     * out the contents of a note in XML format to a specified file name passed in as a parameter. 
+     * This can be easily achieved using the XMLTextWriter class. 
+     * Check out the SDK documentation for sample code on how to do this. */
+    public class ToDoNote : SerialiseToXML
+    {
+        // Fields – the subject matter of the note; the due-date (DateTime); and the priority. 
+        private String subject;
+        private DateTime dueDate;
+        // Implement an appropriate type to represent the various priorities
+        private ToDoNotePriority priority; // priority set for completion
+
+        // Constructor – one constructor which sets all three fields to values passed in as parameters
+        public ToDoNote(string subject, DateTime dueDate, ToDoNotePriority priority)
+        {
+            Subject = subject;
+            DueDate = dueDate;
+            Priority = priority;
+        }
+
+        // Property – read/write properties for each of the three fields
+        public String Subject
+        {
+            get
+            {
+                return subject;
+            }
+            set
+            {
+                subject = value;
+            }
+        }
+
+        public DateTime DueDate
+        {
+            get
+            {
+                return dueDate;
+            }
+            set
+            {
+                dueDate = value;
+            }
+        }
+
+        public ToDoNotePriority Priority
+        {
+            get
+            {
+                return priority;
+            }
+            set
+            {
+                priority = value;
+            }
+        }
+
+        /* Override the inherited ToString method and return a readable string containing full information about the note. 
+        The due date should be displayed as a date without its time portion. */
+        public override string ToString()
+        {
+            return "Subject: " + Subject + "Due Date: " + DueDate.ToString("d") + "Priority Level: " + Priority.ToString();
+        }
+
+
+        /* 2.	Implement a SerialisedXML interface. 
+         * This interface should contain just one method which takes a filename as an input parameter. 
+         * Classes which implement this interface have to provide an implementation of this method 
+         * i.e. they should provide an implementation of a method to write their contents to an XML file. */
+        public void WriteToXML(String filename)
+        {
+            XmlTextWriter tw = new XmlTextWriter(filename, null);
+            tw.Formatting = Formatting.Indented;
+            tw.WriteStartDocument();
+            tw.WriteStartElement("To-Do-Note");
+            tw.WriteElementString("Subject", subject);
+            tw.WriteElementString("Due-Date", dueDate.ToString("d"));
+            tw.WriteElementString("Priority", priority.ToString());
+            tw.WriteEndElement();
+            tw.WriteEndDocument();
+
+            tw.Flush();
+            tw.Close();
+        }
+    }
+
     // *************************Test Class*************************
-    // ************************************************************
     /* 4.	A test class which creates a to-do-list, creates 2 to-do items, adds them to the list, 
      * displays the items in the list, and writes the first item to an XML file “todo.xml” */
     class Program
