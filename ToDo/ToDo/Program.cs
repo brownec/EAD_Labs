8752d459350fc55ede330ce923c3e605a6f8cbd4
brownec <cbrowne77@gmail.com>
2014-10-27 15:12:45 +0000

    Implemented ToDoList Class, properties and methods

--- ToDo/ToDo/Program.cs
+++ ToDo/ToDo/Program.cs
@@ -5,18 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Collections;
+
+// Priority for the ToDoNotePriority collection
+public enum ToDoNotePriority
+{
+    High, Normal, Low
+}
 
 namespace ToDo
 {
     // 1.	Implement a ToDoNote class which represents a to-do note. The class should contain the following members:
-            /* Fields – the subject matter of the note; the due-date (DateTime); and the priority. 
-            Implement an appropriate type to represent the various priorities. */ 
+    public class ToDoNote
+    {
+        // Fields – the subject matter of the note; the due-date (DateTime); and the priority. 
+        private String subject;
+        private DateTime dueDate;
+        // Implement an appropriate type to represent the various priorities
+        private ToDoNotePriority priority; // priority set for completion
 
-            // Constructor – one constructor which sets all three fields to values passed in as parameters
-            // Property – read/write properties for each of the three fields
-            /* Override the inherited ToString method and return a readable string containing full information about the note. 
-            The due date should be displayed as a date without its time portion. */
-
+        // Constructor – one constructor which sets all three fields to values passed in as parameters
+        // Property – read/write properties for each of the three fields
+        /* Override the inherited ToString method and return a readable string containing full information about the note. 
+        The due date should be displayed as a date without its time portion. */
+    }
 
     /* 2.	Implement a SerialisedXML interface. 
      * This interface should contain just one method which takes a filename as an input parameter. 
@@ -33,20 +46,61 @@ namespace ToDo
             A to-do-list contains to-do items. 
             This class should contain the following members: */
 
-           /* Fields - the owner of the list; the collection of to-do notes in the list. 
-            The collection should be a dynamically sized collection e.g. an ArrayList*/
+    // ------------------------------------------------------------------------------------------
+    // -------------------------------------TO-DO-LIST CLASS-------------------------------------
+    // ------------------------------------------------------------------------------------------
+    public class ToDoList
+    {
+        // Fields - the owner of the list; the collection of to-do notes in the list. 
+        private String listOwner; // owner of the ToDoList
+        // The collection should be a dynamically sized collection e.g. an ArrayList
+        private ArrayList notes; 
 
-           /* Constructor – one constructor which sets the name of the owner of the list, 
-             and initialises the collection of to-do notes to empty */
-
+        /* Constructor – one constructor which sets the name of the owner of the list, 
+        and initialises the collection of to-do notes to empty */
+        public ToDoList(String owner)
+        {
+            listOwner = owner;
+            notes = new ArrayList(); // creates a new collection of to-do-notes
+        }
            // Property – a read/write property for the owner field
+        public String listOwner
+        {
+            get
+            {
+                return listOwner;
+            }
+            set
+            {
+                listOwner = value;
+            }
+        }
+        
+        /* Indexer – an indexer which allows the notes in the to-do list to be read (not written) 
+        using array type indexing 
+        i.e. an object which is an instance of the ToDoList can be iterated over like an array. */
+        public ToDoNote this[int i]
+        {
+            get
+            {
+                if((i >= 0) && (i < notes.Count))
+                {
+                    return (ToDoNote)notes[i];
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Index!");
+                }
+            }
+        }
 
-           /* Indexer – an indexer which allows the notes in the to-do list to be read (not written) 
-              using array type indexing 
-              i.e. an object which is an instance of the ToDoList can be iterated over like an array. */
-
-          //  Add – a method which allows a note to be added to the list (at the next available position)
-
+        //  Add – a method which allows a note to be added to the list (at the next available position)
+        public void AddToDoNote(ToDoNote note)
+        {
+            notes.Add(note);
+        }
+    }
+        
     
     // ************************************************************
     // *************************Test Class*************************
