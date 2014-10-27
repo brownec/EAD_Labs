30a109fcdcd541fbdc41631f6358a763af63f2ca
brownec <cbrowne77@gmail.com>
2014-10-27 13:19:21 +0000

    Implemented BankAccount class, initial properties and initial methods

---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,99 @@
+﻿// Cliff Browne - X00014810
+// EAD1 CA1 Example Program - Bank Account and Unit Tests
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /* You are required to design and implement a simple banking system in C# follows:
+1.	Implement a BankAccount class to represent a generic bank account. */
+    public class BankAccount
+    {
+        // Each account has an account number, balance, and abstract methods to make deposits and withdrawals from the account. 
+        // More specifically the class should contain the following items: 
+
+        /* Fields:
+        The bank account number (alpha-numeric)
+        The balance on the account */
+        private string accountNumber;
+        private double accountBalance;
+
+        /* Properties:
+        A read-only property for the account number field
+        A read-write property for the balance field */
+        public String AccountNumber
+        {
+            get
+            {
+                return accountNumber;
+            }
+        }
+
+        public double AccountBalance
+        {
+            get
+            {
+                return accountBalance;
+            }
+        }
+        /* Constructor:
+        One constructor which sets the account number to the value of an input parameter, and sets the balance to zero */
+        public BankAccount(String accNum, double accBal)
+        {
+            this.accountNumber = accNum;
+            this.accountBalance = 0; // set the account balance to zero
+        }
+
+        /* Abstract Methods:
+        MakeDeposit -allow an amount of money to be deposited in the account
+        MakeWithdrawal - allow an amount of money to be withdrawn from the account */
+        public void makeDeposit(double depAmt)
+        {
+            if (depAmt > 0)
+            {
+                accountBalance += depAmt;
+            }
+            else
+            {
+                throw new ArgumentException("Deposit amount must be greater than zero.");
+            }
+        }
+
+        public void makeWithdrawl(double wdAmt)
+        {
+            // 1. check to ensure withdrawl amount greater than zero
+            if (wdAmt > 0)
+            {
+                // 2. check to ensure account balance is greater than withdrawl amount
+                if (accountBalance > wdAmt)
+                {
+                    accountBalance -= wdAmt;
+                }
+                else
+                {
+                    // 2. exception thrown if account balance less than withdrawl amount
+                    throw new ArgumentException("Insufficient Funds. Please enter another amount to withdraw.");
+                }
+            }
+            else
+            {
+                // 1. exception thrown if withdrawl amount less than or equal to zero
+                throw new ArgumentException("Withdrawl Amount must be greater than zero.");
+            }
+        }
+    }
+     
+
+
+    // Test Class
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("*****EAD CA1 Example Program - Bank Account*****")
+        }
+    }
+}
---
+++ UnitTest_Bank/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest_Bank;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Test the Deposit Method
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BankAccount bank1 = new BankAccount("99-10-10", "cb123456");
+            bank1.makeDeposit(100.00);
+            bank1.makeDeposit(200.00);
+            Assert.AreEqual(bank1.Balance, 300.00);
+        }
+
+        // Test Exception thrown if invalid overdraft amount
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateBankAccountWithInvalidOverdraft()
+        {
+            // -1000 overdraft limit/overdraft cannot be a negative amount
+            BankAccount bank2 = new BankAccount("99-20-96", "cb987654", -1000)
+        }
+
+        // Test for Deposit and Withdrawl
+        [TestMethod]
+        public void TestDepAndWD()
+        {
+            BankAccount bank3 = new BankAccount("99-11-00", "cb000000", 1000)
+            bank3.makeDeposit(100.00);
+            bank3.makeWithdrawl(90.00);
+            Assert.AreEqual(bank3.Balance, 10.00);
+        }
+
+        // Test TransactionHistory contains the number of actual Transactions
+        [TestMethod]
+        public void TestTransactionHistory()
+        {
+            BankAccount bank4 = new BankAccount("99-00-10", "cb456789");
+            bank4.makeDeposit(500.00);
+            bank4.makeDeposit(500.00);
+            bank4.makeWithdrawl(600.00);
+            Assert.AreEqual(bank4.Balance, 400.00);
+            // Test the number of transactions on the account
+            CollectionAssert.AreEqual(bank4.TransactionHistory, new List<double>()
+            {
+                500.00, 500.00, -600
+            });
+        }
+    }
+}
