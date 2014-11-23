10e82270af70eb3db15554e0a863140e442ce80e
brownec <cbrowne77@gmail.com>
2014-11-23 16:12:10 +0000

    Implemented HomeController changes to add Calculate and Confirm methods

--- EADCAEx1/EADCAEx1/Controllers/HomeController.cs
+++ EADCAEx1/EADCAEx1/Controllers/HomeController.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EADCAEx1.Models;
 
 namespace EADCAEx1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Calculate()
         {
-            return View();
+            ViewBag.InstanceSize = new SelectList(AzureCloudServices.InstanceSizeDescription);
+            return View(new AzureCloudServices() { NumInstances = 2});
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Calculate(AzureCloudServices acs)
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Confirm", acs);
+            }
+            else
+            {
+                ViewBag.InstanceSize = new SelectList(AzureCloudServices.InstanceSizeDescription);
+                return View(acs);
+            }
+        }    
+ 
+        public ActionResult Confirm(AzureCloudServices acs)
         {
             ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(acs);
         }
     }
 }
