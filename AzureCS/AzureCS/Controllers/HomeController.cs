bb297f143f55eb5cc410d127471221965ec8e376
brownec <cbrowne77@gmail.com>
2014-11-24 18:40:07 +0000

    Implemented the HomeController methods, views and edited the route_config and .layout.cshtml pages

--- AzureCS/AzureCS/Controllers/HomeController.cs
+++ AzureCS/AzureCS/Controllers/HomeController.cs
@@ -3,28 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AzureCS.Models;
 
 namespace AzureCS.Controllers
 {
+    [HttpGet]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Calculate()
         {
-            return View();
+            return View(new AzureCloudServices() { IDS = InstanceSizeDescription.Medium, NumInstances = 2 });
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Calculate(AzureCloudServices acs)
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
+            if (ModelState.IsValid)
+            {
+                return (RedirectToAction("Confirm", acs));
+            }
+            else
+            {
+                return (RedirectToAction("Calculate", acs));
+            }
+            // return View(); 
         }
 
-        public ActionResult Contact()
+        public ActionResult Confirm(AzureCloudServices acs)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            // ViewBag.Message = "Your contact page.";
+            TimeZone tz = TimeZone.CurrentTimeZone;
+            String zone = tz.StandardName.ToString();
+            String time = DateTime.Now.ToString();
+            ViewBag.AzureCS = new String[] { zone, time };
+            return View(acs);
         }
     }
 }
