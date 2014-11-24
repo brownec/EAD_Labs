33e181db9d03b018192a0253d1c15a081341b274
brownec <cbrowne77@gmail.com>
2014-11-24 17:42:51 +0000

    Edit Calculate and Confirm views.  Add Date/Time to Confirm view and changed colors and changed .layout.cshtml

--- EnumCA1/EnumCA1/Controllers/HomeController.cs
+++ EnumCA1/EnumCA1/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using EnumCA1.Models;
+using System;
 using System.Web.Mvc;
-using EnumCA1.Models;
 
 namespace EnumCA1.Controllers
 {
@@ -32,6 +29,10 @@ namespace EnumCA1.Controllers
         public ActionResult Confirm(AzureServices azs)
         {
             ViewBag.Message = "Your contact page.";
+            TimeZone tz = TimeZone.CurrentTimeZone;
+            String zone = tz.StandardName.ToString();
+            String time = DateTime.Now.ToString();
+            ViewBag.EnumCA1 = new String[] {zone, time };
             return View(azs);
         }
     }
